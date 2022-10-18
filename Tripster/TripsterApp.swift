259d4a21643ee395a689c7f283511811861c426c
vincentf2001 <vincentf8989@gmail.com>
2022-10-18 15:16:10 +0700

    Initial Commit

---
+++ Tripster/TripsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsterApp.swift
+//  Tripster
+//
+//  Created by vincent fernando on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TripsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
